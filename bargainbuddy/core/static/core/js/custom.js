784669c35a1e0b374864193894a93a72d66adaa5
MHSiam1 <2223011084@uttarauniversity.edu.bd>
2024-01-07 23:01:37 +0600

    static file added

---
+++ bargainbuddy/core/static/core/js/custom.js
@@ -0,0 +1,9 @@
+Shery.mouseFollower({
+    //Parameters are optional.
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
+  function showAlert() {
+    alert("Hello, this is an alert!");
+}
