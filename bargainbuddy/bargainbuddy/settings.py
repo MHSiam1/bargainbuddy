775936d9a44ba2052e19a45110ccde6cc0309ea7
MHSiam1 <2223011084@uttarauniversity.edu.bd>
2024-01-07 20:42:31 +0600

    from updated

--- bargainbuddy/bargainbuddy/settings.py
+++ bargainbuddy/bargainbuddy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,9 +119,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# Static files (CSS, JavaScript, images)
+
+
+
+
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "core/static",
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
+
+
+
+
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- bargainbuddy/core/urls.py
+++ bargainbuddy/core/urls.py
@@ -1,5 +1,22 @@
+from django.conf import settings
+
+
+
+
+
+
+
+
+from django.conf.urls.static import static
+
+
+
 from django.contrib.auth import views as auth_views
 from django.urls import path
+
+from .views import index
+
+
 
 from . import views
 from .forms import LoginForm
@@ -7,6 +24,9 @@ from .forms import LoginForm
 app_name = 'core'
 
 urlpatterns = [
+    
+    path('', index, name='index'),
+
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
