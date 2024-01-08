2fc4b3e621b939542381bac1f27f4a76b693b30b
MHSiam1 <2223011084@uttarauniversity.edu.bd>
2024-01-08 18:52:30 +0600

    Frontend Updated

--- bargainbuddy/core/static/core/js/custom.js
+++ bargainbuddy/core/static/core/js/custom.js
@@ -4,6 +4,23 @@ Shery.mouseFollower({
     ease: "cubic-bezier(0.23, 1, 0.320, 1)",
     duration: 1,
   });
-  function showAlert() {
-    alert("Hello, this is an alert!");
-}
+
+// var timeline = gsap.timeline()
+// timeline.from("#loader h2", {
+//   x:90,
+//   opacity:0,
+//   duration:2,
+//   stagger:0.1
+// })
+// timeline.to("#loader h2", {
+//   x:-90,
+//   opacity:0,
+//   duration:1,
+//   stagger:0.1
+// })
+// timeline.to("#loader", {
+//   opacity:0
+// })
+// timeline.to("#loader", {
+//   display:"none"
+// })
